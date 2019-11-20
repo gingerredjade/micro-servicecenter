03b342302fb9c37703a7f23f5818a465ebb5ec22
jianghongyu <jianghy@gis.nci>
2019-11-20 09:32:32 +0800

    完善上传文件功能 环境变量模式postman_collection

--- gis-service-core/src/main/java/com/nci/gis/GisServiceCoreApplication.java
+++ gis-service-core/src/main/java/com/nci/gis/GisServiceCoreApplication.java
@@ -5,13 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.MultipartConfigElement;
 
 /**
  * 启动主类
@@ -29,23 +26,6 @@ public class GisServiceCoreApplication {
         SpringApplication.run( GisServiceCoreApplication.class, args );
 		logger.info("[GisServiceCoreApplication微服务] 已启动");
     }
-
-	/**
-	 * 配置上传文件大小
-	 *
-	 * @return 返回值
-	 */
-	@Bean
-    public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-
-		// 单个数据大小
-		factory.setMaxFileSize("102400KB");
-
-		// 总上传数据大小
-		factory.setMaxRequestSize("102400KB");
-		return factory.createMultipartConfig();
-	}
 
 
 }
